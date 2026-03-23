7251e29f8e3fe2cb6ef4e44a014b462d00202ad4
Automizer <git@automizer.es>
2026-03-23 12:53:15 +0000

    Automated commit for version v0.0.29

--- custom_components/automizer/__init__.py
+++ custom_components/automizer/__init__.py
@@ -28,31 +28,32 @@ import re,uuid,time,os,logging,yaml
 _LOGGER=logging.getLogger(__name__)
 _PLATFORMS=[Platform.LIGHT,Platform.SENSOR,Platform.SWITCH,Platform.NUMBER]
 async def async_setup_entry(hass,entry):
-	Z='decimals';Y='0x0105';X='automizer';R='_status';Q='refresh_seconds';J='name';B=hass;A=entry
+	Z='decimals';Y='0x0105';S='_status';R='refresh_seconds';K='name';B=hass;A=entry
 	if DOMAIN not in B.data:B.data[DOMAIN]={}
 	if A.entry_id in B.data[DOMAIN]:_LOGGER.warning(f"La entrada {A.entry_id} ya está configurada.");return False
-	B.data[DOMAIN][A.entry_id]={};B.data[DOMAIN][A.entry_id]={_C:[],_A:[],_D:[],_F:[],_G:[],_H:[],_I:[],_J:[],_E:[],_B:None};a=A.data[CONF_EXPORT];b=a.splitlines();c=re.compile('(?P<deviceName>[a-zA-Z0-9_-]+)\\s(?P<deviceType>[a-zA-Z0-9_-]+)\\s(?P<deviceId>[0-9A-F,x]+)');N=os.path.join(os.path.dirname(__file__),'configuration.yaml')
-	try:K=await B.async_add_executor_job(read_yaml_file,N)
-	except FileNotFoundError:_LOGGER.info(f"No se encontró el archivo {N}.");K={}
-	except yaml.YAMLError as d:_LOGGER.error(f"Error al leer el archivo {N} YAML: {d}");K={}
-	S=K.get(X,{}).get(_C,[]);_LOGGER.info(f"Configuración de números cargada: {S}");G=K.get(X,{}).get('analog',[]);_LOGGER.info(f"Configuración de sensores analógicos cargada: {G}")
-	for H in b:
-		if H.startswith('_'):continue
-		O=c.match(H)
-		if O:
-			_LOGGER.info(f"READ EXPORT LINE: {H}");e=O.group('deviceName');D=O.group('deviceId');E=A.data[CONF_CU_NAME]+'_'+e
+	B.data[DOMAIN][A.entry_id]={};B.data[DOMAIN][A.entry_id]={_C:[],_A:[],_D:[],_F:[],_G:[],_H:[],_I:[],_J:[],_E:[],_B:None};a=A.data[CONF_EXPORT];b=a.splitlines();c=re.compile('(?P<deviceName>[a-zA-Z0-9_-]+)\\s(?P<deviceType>[a-zA-Z0-9_-]+)\\s(?P<deviceId>[0-9A-F,x]+)');L=os.path.join(os.path.dirname(__file__),'configuration.yaml');_LOGGER.info(f"Leyendo configuración desde: {L}")
+	try:F=await B.async_add_executor_job(read_yaml_file,L)
+	except FileNotFoundError:_LOGGER.info(f"No se encontró el archivo {L}.");F={}
+	except yaml.YAMLError as d:_LOGGER.error(f"Error al leer el archivo {L} YAML: {d}");F={}
+	if F is None:_LOGGER.warning('El archivo de configuración está vacío o es inválido.');F={}
+	_LOGGER.info(f"config_data completo: {F}");O=F.get('automizer',{});_LOGGER.info(f"Sección automizer: {O}");T=O.get(_C,[]);_LOGGER.info(f"Configuración de números cargada: {T}");H=O.get('analog',[]);_LOGGER.info(f"Configuración de sensores analógicos cargada: {H}")
+	for I in b:
+		if I.startswith('_'):continue
+		P=c.match(I)
+		if P:
+			_LOGGER.info(f"READ EXPORT LINE: {I}");e=P.group('deviceName');D=P.group('deviceId');E=A.data[CONF_CU_NAME]+'_'+e
 			if D.startswith('0x0101'):C=s.InelsBinarySensor(E,D);B.data[DOMAIN][A.entry_id][_E].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
 			if D.startswith('0x0102'):C=sw.InelsSwitch(E,D);B.data[DOMAIN][A.entry_id][_D].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
 			elif D.startswith('0x0104'):C=l.InelsLight(E,D);B.data[DOMAIN][A.entry_id][_F].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
-			elif D.startswith(Y)and'%'not in H:P=E;_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {P}");F=next((A.get(Q,0)for A in G if A[J]==P),0);_LOGGER.info(f"[TEMP] {P}: refresh_seconds={F}");C=s.InelsTemperatureSensor(E,D,F);B.data[DOMAIN][A.entry_id][_G].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
-			elif D.startswith(Y)and'%'in H:T=E;_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {T}");F=next((A.get(Q,0)for A in G if A[J]==T),0);C=s.InelsHumiditySensor(E,D,F);B.data[DOMAIN][A.entry_id][_H].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
+			elif D.startswith(Y)and'%'not in I:Q=E;_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {Q}");G=next((A.get(R,0)for A in H if A[K]==Q),0);_LOGGER.info(f"[TEMP] {Q}: refresh_seconds={G}");C=s.InelsTemperatureSensor(E,D,G);B.data[DOMAIN][A.entry_id][_G].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
+			elif D.startswith(Y)and'%'in I:U=E;_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {U}");G=next((A.get(R,0)for A in H if A[K]==U),0);C=s.InelsHumiditySensor(E,D,G);B.data[DOMAIN][A.entry_id][_H].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
 			elif D.startswith('0x0203'):C=sw.InelsSwitch(E,D);B.data[DOMAIN][A.entry_id][_D].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
-			elif D.startswith('0x0202'):U=E;L=next((A.get(Z,0)for A in S if A[J]==U),0);_LOGGER.info(f"Buscando decimales en el archivo de configuracion: {U}");_LOGGER.info(f"Creando entero con decimales: {L}");C=n.InelsNumber(E,D,L);B.data[DOMAIN][A.entry_id][_C].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
-			elif D.startswith('0x0108'):M=E;_LOGGER.info(f"Buscando decimales en el archivo de configuracion: {M}");L=next((A.get(Z,0)for A in G if A[J]==M),0);_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {M}");F=next((A.get(Q,0)for A in G if A[J]==M),0);C=s.InelsAnalogSensor(E,D,L,F);B.data[DOMAIN][A.entry_id][_I].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
+			elif D.startswith('0x0202'):V=E;M=next((A.get(Z,0)for A in T if A[K]==V),0);_LOGGER.info(f"Buscando decimales en el archivo de configuracion: {V}");_LOGGER.info(f"Creando entero con decimales: {M}");C=n.InelsNumber(E,D,M);B.data[DOMAIN][A.entry_id][_C].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
+			elif D.startswith('0x0108'):N=E;_LOGGER.info(f"Buscando decimales en el archivo de configuracion: {N}");M=next((A.get(Z,0)for A in H if A[K]==N),0);_LOGGER.info(f"Buscando intervalo de actualizacion en el archivo de configuracion: {N}");G=next((A.get(R,0)for A in H if A[K]==N),0);C=s.InelsAnalogSensor(E,D,M,G);B.data[DOMAIN][A.entry_id][_I].append(C);B.data[DOMAIN][A.entry_id][_A].append(C)
 		else:continue
-	V=s.InelsTextSensor(A.data[CONF_CU_NAME]+R,A.data[CONF_CU_NAME]+R);B.data[DOMAIN][A.entry_id][_J].append(V);W=s.InelsBinarySensor(A.data[CONF_CU_NAME]+'_client_connected',A.data[CONF_CU_NAME]+R);B.data[DOMAIN][A.entry_id][_E].append(W);await B.config_entries.async_forward_entry_setups(A,[Platform.SWITCH,Platform.LIGHT,Platform.NUMBER,Platform.SENSOR]);f=inelsObj.InelsCentralUnit(A.data[CONF_CU_NAME],A.data[CONF_HOST],A.data[CONF_PORT]);I=B.data[DOMAIN][A.entry_id];I[_B]=ic.InelsClient2(f,I[_A],V,W)
-	for g in I[_A]:g.ic=I[_B]
-	I[_B].start();return True
+	W=s.InelsTextSensor(A.data[CONF_CU_NAME]+S,A.data[CONF_CU_NAME]+S);B.data[DOMAIN][A.entry_id][_J].append(W);X=s.InelsBinarySensor(A.data[CONF_CU_NAME]+'_client_connected',A.data[CONF_CU_NAME]+S);B.data[DOMAIN][A.entry_id][_E].append(X);await B.config_entries.async_forward_entry_setups(A,[Platform.SWITCH,Platform.LIGHT,Platform.NUMBER,Platform.SENSOR]);f=inelsObj.InelsCentralUnit(A.data[CONF_CU_NAME],A.data[CONF_HOST],A.data[CONF_PORT]);J=B.data[DOMAIN][A.entry_id];J[_B]=ic.InelsClient2(f,J[_A],W,X)
+	for g in J[_A]:g.ic=J[_B]
+	J[_B].start();return True
 async def async_get_options_flow(config_entry):from.options_flow import AutomizerOptionsFlowHandler as A;return A(config_entry)
 async def async_unload_entry(hass,entry):
 	'Descargar una entrada de configuración.';C=hass;B=entry;A=C.data[DOMAIN][B.entry_id]
